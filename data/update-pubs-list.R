0403f16d28d1e7e3e550181daf21b5648cc383c3
cbrown5 <christo.j.brown@gmail.com>
2017-11-17 14:30:03 +1000

    update pubs

---
+++ data/update-pubs-list.R
@@ -0,0 +1,31 @@
+# Extract data from google scholar, save to spreadsheet
+
+library(scholar)
+library(dplyr)
+
+datbase <- read.csv("~/Documents/Webpage/webpage/_data/pubs.csv")
+
+thisid <- "1qG6yFMAAAAJ"
+datin <- get_publications(id = thisid)
+# ikeep <- !(datin$cid %in% datbase$cid)
+# dat <- datin[ikeep,]
+dat <- datin
+
+iord <- order(dat$year, decreasing = TRUE)
+dats <- dat[iord,]
+
+auths <- dats$author %>% as.character() %>%
+	strsplit(split = ",")
+
+dats$first <- grepl("Brown",lapply(auths, function(x) x[1])) %>%
+	as.character() %>% tolower()
+
+dats$url <- paste0("https://scholar.google.com.au/citations?view_op=view_citation&hl=en&user=",thisid,"&citation_for_view=",thisid,":",dats$pubid)
+
+dats <- select(dats, -number)
+ikeep <- !(dats$journal == "")
+dats <- dats[ikeep,]
+dats$year[is.na(dats$year)] <- "In press"
+dats <- arrange(dats, desc(year))
+
+write.csv(dats, "/Users/s2973410/Documents/Webpage/webpagev2/_data/pubs.csv", row.names = F)
